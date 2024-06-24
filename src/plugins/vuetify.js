550fc62a0e219a5f0a134d90015ca1c58b08423e
robert-osses <r.osses.v@gmail.com>
2024-06-24 15:49:20 -0400

    ROV ajustes de usuario y seguridad 20240624

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,33 +1,37 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.css";
+import "vuetify/dist/vuetify.min.css";
 import es from "vuetify/es5/locale/es";
 
 Vue.use(Vuetify);
 
+const light = {
+      primary: "#B71C1C",
+      secondary: "#424242",
+      info: "#2196F3",
+      success: "#008000",
+      error: "#FF5252",
+      background: "#429191"
+}
+
+const dark = {
+      background: "#000000",
+      primary: "#fb2424",
+      secondary: "#808080",
+      info: "#2196F3",
+      success: "#4CAF50",
+      error: "#FF5252",
+}
+
 export default new Vuetify({
   theme: {
-    options: {
-      customProperties: true
-    },
     themes: {
-      light: {
-        background: "#ededed",
-        primary: "#B71C1C",
-        secondary: "#424242",
-        // accent: "#82B1FF",
-        // error: "#FF5252",
-        info: "#2196F3",
-        success: "#4CAF50",
-        // warning: "#FFC107",
-        // primaryBanner: "#202020"
-      }
-    }
+        light,
+        dark,
+    },
   },
   lang: {
     locales: { es },
     current: "es"
   }
 });
-
-
